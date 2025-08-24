f691237a3d17181b79e17a8ef0fb1b29f434586b
sean boose <seanboose@gmail.com>
2025-08-24 16:24:22 -0700

    hit deployed backend

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,26 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import { Welcome } from '../welcome/welcome';
+import { useEffect } from 'react';
+import { data } from 'react-router';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
   ];
 }
 
 export default function Home() {
+  useEffect(() => {
+    const callback = async () => {
+      const res = await fetch(
+        'https://personal-website-backend-fvac.onrender.com/api/hello'
+      );
+      const payload: { message: string } = await res.json();
+      console.log(payload.message);
+    };
+    callback();
+  }, []);
+
   return <Welcome />;
 }
