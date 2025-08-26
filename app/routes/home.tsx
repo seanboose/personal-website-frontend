12c592b1f91109751be0bc8d4920a9ca7588b70b
sean boose <seanboose@gmail.com>
2025-08-26 12:24:31 -0700

    connect to local api, add env files

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,6 @@
 import type { Route } from './+types/home';
 import { Welcome } from '../welcome/welcome';
 import { useEffect } from 'react';
-import { data } from 'react-router';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,9 +12,12 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   useEffect(() => {
     const callback = async () => {
-      const res = await fetch(
-        'https://personal-website-backend-fvac.onrender.com/api/hello'
-      );
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/hello`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
       const payload: { message: string } = await res.json();
       console.log(payload.message);
     };
