a676af932c4f34dbf131d30fb8ffb2377f09db3b
sean boose <seanboose@proton.me>
2025-11-22 15:58:33 -0700

    add and use toggle component

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -12,9 +12,32 @@ export const Button = ({
   return (
     <button type={type} onClick={onClick}>
       <div className="group  cursor-pointer py-1 hover:border-y-2 hover:py-0.5 hover:text-text-selected border-background-selected">
-        <div className="flex flex-row items-center p-1 bg-nav-selected group-hover:bg-background-selected">
+        <div className="flex flex-row items-center p-1 bg-nav-unselected group-hover:bg-background-selected">
           <div
             className={`w-3 h-3 mx-1 bg-text-body group-hover:bg-text-selected`}
+          />
+          {children}
+        </div>
+      </div>
+    </button>
+  );
+};
+
+export const Toggle = ({
+  children,
+  onClick,
+  active,
+}: {
+  children: ReactNode;
+  onClick: () => void;
+  active: boolean;
+}) => {
+  return (
+    <button type="button" onClick={onClick}>
+      <div className="group cursor-pointer hover:text-text-selected border-background-selected">
+        <div className="flex flex-row items-center p-1 group-hover:bg-background-selected">
+          <div
+            className={`w-3 h-3 mx-1 ${active ? 'bg-text-body group-hover:bg-text-selected' : 'border-2 border-text-body group-hover:border-text-selected'}`}
           />
           {children}
         </div>
--- app/routes/_layout.landwhale.tsx
+++ app/routes/_layout.landwhale.tsx
@@ -1,8 +1,10 @@
 import { useCallback, useState } from 'react';
 
-import { Button } from '~/components/button';
+import { Toggle } from '~/components/button';
 import { HeaderedSurface } from '~/components/headeredSurface';
+import { RowStart } from '~/components/rowStart';
 import { Surface } from '~/components/surface';
+import { H1 } from '~/components/typography';
 import { getS3Url } from '~/shared/aws';
 
 export const loader = async () => {
@@ -29,41 +31,53 @@ export default function Landwhale() {
     [visibility],
   );
   return (
-    <div className="flex flex-col md:flex-row gap-4 min-h-60">
-      <Surface className="flex-1">
-        <div className="flex flex-col gap-2">
-          <p>
-            oh no, the poor landwhale is about to embark on a harrowing mission
-            to save all existence from a mysterious plague that has fallen upon
-            the global subconscious and forgot to equip all of its upgrades!
-          </p>
-          <p>
-            press the buttons to equip the landwhale's upgrades and prepare for
-            the mission!
-          </p>
+    <div className="flex flex-col gap-4">
+      <H1>equip the landwhale</H1>
+      <div className="flex flex-col md:flex-row gap-4 min-h-60">
+        <RowStart />
+        <Surface className="flex-1">
           <div className="flex flex-col gap-2">
-            {upgrades.map((upgrade) => (
-              <Button onClick={() => toggleItem(upgrade)}>{upgrade}</Button>
-            ))}
+            <p>
+              oh no, the poor landwhale is about to embark on a harrowing
+              mission to save all existence from a mysterious plague that has
+              fallen upon the global subconscious and it forgot to equip all of
+              its upgrades!
+            </p>
+            <p>
+              press the buttons to equip the landwhale's upgrades and prepare
+              for the mission!
+            </p>
+            <div className="flex flex-col">
+              {upgrades.map((upgrade) => (
+                <Toggle
+                  active={visibility[upgrade]}
+                  onClick={() => toggleItem(upgrade)}
+                >
+                  {upgrade.replace('-', ' ')}
+                </Toggle>
+              ))}
+            </div>
           </div>
-        </div>
-      </Surface>
-      <HeaderedSurface header="Landwhale Specimen" className="flex-1">
-        <div className="relative w-full h-full">
-          <img
-            className={imgClassname}
-            alt="base"
-            src={getS3Url('/assets/landwhale/base.png')}
-          />
-          {upgrades.map((upgrade) => (
-            <img
-              className={`${imgClassname} ${visibility[upgrade] ? '' : 'invisible'}`}
-              alt={upgrade}
-              src={getS3Url(`/assets/landwhale/${upgrade}.png`)}
-            />
-          ))}
-        </div>
-      </HeaderedSurface>
+        </Surface>
+        <HeaderedSurface header="Current Loadout" className="flex-1">
+          <div className="h-full flex items-center justify-center">
+            <div className="relative bg-nav-unselected w-xs aspect-square">
+              <img
+                className={imgClassname}
+                alt="base"
+                src={getS3Url('/assets/landwhale/base.png')}
+              />
+              {upgrades.map((upgrade) => (
+                <img
+                  className={`${imgClassname} ${visibility[upgrade] ? '' : 'invisible'}`}
+                  alt={upgrade}
+                  src={getS3Url(`/assets/landwhale/${upgrade}.png`)}
+                />
+              ))}
+            </div>
+          </div>
+        </HeaderedSurface>
+      </div>
     </div>
   );
 }
