90a7b677ed1ea7920c100d64d5a9eedd0b8a9502
sean boose <seanboose@proton.me>
2025-12-10 16:23:14 -0700

    add more e2e tests for landwhale page

--- e2e/navBar.spec.ts
+++ e2e/navBar.spec.ts
@@ -4,8 +4,8 @@ test.describe('can nav to', async () => {
   test('landwhale', async ({ page }) => {
     await page.goto('/');
 
-    // await expect(page.getByRole('heading', { name: 'Installation' }));
     await page.getByRole('link', { name: 'landwhale' }).click();
+
     await expect(
       page.getByRole('heading', { name: 'equip the landwhale' }),
     ).toBeVisible();
